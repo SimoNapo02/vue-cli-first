22a7f9872f4534c658d51597bdb7a2c19f1e6d29
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-31 20:00:24 +0200

    first commit

---
+++ src/components/BaseTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>La mia prima volta con Vue CLI</h1>
+</template>
+
+<script>
+export default {
+    name: 'BaseTitle'
+}
+</script>
+
+<style>
+    h1{
+        color: rgb(79, 255, 137);
+    }
+</style>
